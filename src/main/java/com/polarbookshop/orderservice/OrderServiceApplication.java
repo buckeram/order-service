f9847060e0d78ba8d7298631e2b007a0af603adf
buckeram <buckeram@gmail.com>
2025-05-13 00:04:03 +0100

    Chapter 8: Reactive Spring

--- src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
+++ src/main/java/com/polarbookshop/orderservice/OrderServiceApplication.java
@@ -1,9 +1,11 @@
-package com.polarbookshop.order_service;
+package com.polarbookshop.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
